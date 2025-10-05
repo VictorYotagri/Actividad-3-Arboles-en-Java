f3761c491517216d73b36481e3fa9a2f682cd846
VictorYotagri <vyotagri@gmail.com>
2025-10-05 13:52:33 -0500

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+// Arboles binarios de busqueda
+// Implementar las operaciones de inserción, búsqueda y recorrido en orden (in-order traversal).
+class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
+}
+
+class Tree{
+    Node root;
+
+    Tree() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+        return root;
+    }
+
+    boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    boolean searchRec(Node root, int key) {
+        if (root == null)
+            return false;
+        if (root.key == key)
+            return true;
+        return key < root.key ? searchRec(root.left, key) : searchRec(root.right, key);
+    }
+
+    void inorder() {
+        inorderRec(root);
+    }
+
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.key + " ");
+            inorderRec(root.right);
+        }
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+
+    }
+}
